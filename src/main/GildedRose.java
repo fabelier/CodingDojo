eda01272fdbcf1e5d1e9ce86f5d86b80ccfebb2d
Guillaume Turri <guillaume.turri@gmail.com>
2013-06-26 20:35:03 +0200

    started to refactor
    * Added tests before we touched the main code (Added a kind of toString for the first end to end test)
    * Got rid of the "changeQuality" duplication

--- src/main/GildedRose.java
+++ src/main/GildedRose.java
@@ -6,103 +6,121 @@ import java.util.List;
 
 public class GildedRose {
 
-	private static List<Item> items = null;
+  private static List<Item> _items = null;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
 
-        System.out.println("OMGHAI!");
+    System.out.println("OMGHAI!");
 
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
+    _items = new ArrayList<Item>();
+    _items.add(new Item("+5 Dexterity Vest", 10, 20));
+    _items.add(new Item("Aged Brie", 2, 0));
+    _items.add(new Item("Elixir of the Mongoose", 5, 7));
+    _items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+    _items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+    _items.add(new Item("Conjured Mana Cake", 3, 6));
 
-        updateQuality();
-}
+    updateQuality(_items);
+    System.out.println(print(_items));
+  }
+
+  public static String print(List<Item> items){
+	  String result = "";
+	  for(Item item : items){
+		  result += print(item) + "\n";
+	  }
+	  return result;
+  }
+
+  private static String print(Item item){
+	  String result = "";
+	  result += item.name + "-" + item.sellIn + "-" + item.quality;
+	  return result;
+  }
+
+  private static void changeQuality(int delta, Item item){
+	  item.setQuality(item.getQuality() + delta);
+  }
 
 
-
-    public static void updateQuality()
+  public static void updateQuality(List<Item> items)
+  {
+    for (int i = 0; i < items.size(); i++)
     {
-        for (int i = 0; i < items.size(); i++)
+      if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+      {
+        if (items.get(i).getQuality() > 0)
         {
-            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-            {
-                if (items.get(i).getQuality() > 0)
-                {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - 1);
-                    }
-                }
-            }
-            else
-            {
-                if (items.get(i).getQuality() < 50)
-                {
-                    items.get(i).setQuality(items.get(i).getQuality() + 1);
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getSellIn() < 11)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-
-                        if (items.get(i).getSellIn() < 6)
-                        {
-                            if (items.get(i).getQuality() < 50)
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-            {
-                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
-            }
-
-            if (items.get(i).getSellIn() < 0)
-            {
-                if (!"Aged Brie".equals(items.get(i).getName()))
-                {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
-                    {
-                        if (items.get(i).getQuality() > 0)
-                        {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
-                            {
-                                items.get(i).setQuality(items.get(i).getQuality() - 1);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
-                    }
-                }
-                else
-                {
-                    if (items.get(i).getQuality() < 50)
-                    {
-                        items.get(i).setQuality(items.get(i).getQuality() + 1);
-                    }
-                }
-            }
+          if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+          {
+        	  changeQuality(-1, items.get(i));
+          }
         }
+      }
+      else
+      {
+        if (items.get(i).getQuality() < 50)
+        {
+        	changeQuality(1, items.get(i));
+
+          if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+          {
+            if (items.get(i).getSellIn() < 11)
+            {
+              if (items.get(i).getQuality() < 50)
+              {
+            	  changeQuality(1, items.get(i));
+              }
+            }
+
+            if (items.get(i).getSellIn() < 6)
+            {
+              if (items.get(i).getQuality() < 50)
+              {
+            	  changeQuality(1, items.get(i));
+              }
+            }
+          }
+        }
+      }
+
+      if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+      {
+        items.get(i).setSellIn(items.get(i).getSellIn() - 1);
+      }
+
+      if (items.get(i).getSellIn() < 0)
+      {
+        if (!"Aged Brie".equals(items.get(i).getName()))
+        {
+          if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+          {
+            if (items.get(i).getQuality() > 0)
+            {
+              if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+              {
+            	  changeQuality(-1, items.get(i));
+              }
+            }
+          }
+          else
+          {
+            items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
+          }
+        }
+        else
+        {
+          if (items.get(i).getQuality() < 50)
+          {
+        	  changeQuality(1, items.get(i));
+          }
+        }
+      }
     }
+  }
 
 
 }
--- src/tests/T_GildedRose.java
+++ src/tests/T_GildedRose.java
@@ -1,13 +1,67 @@
 package tests;
 import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
+import main.GildedRose;
+import main.Item;
 
 
 public class T_GildedRose {
 	@Test
 	public void testTheTruth() {
-		assertTrue(true);
+		// Create a stream to hold the output
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		// IMPORTANT: Save the old System.out!
+		PrintStream old = System.out;
+		// Tell Java to use your special stream
+		System.setOut(ps);
+		// Print some output: goes to your special stream
+		GildedRose.main(null);
+
+		// Put things back
+		System.out.flush();
+		System.setOut(old);
+		// Show what happened
+		assertEquals(-312259383, baos.toString().hashCode());
+		System.out.println(baos.toString().hashCode());
 	}
+
+	@Test
+	public void testStandardObject(){
+		int initialQuality = 20;
+		int initialSellin = 10;
+
+		Item item = new Item("+5 Dexterity Vest", initialSellin, initialQuality);
+		this.updateItem(item);
+	    assertEquals(initialSellin-1, item.getSellIn());
+	    assertEquals(initialQuality-1, item.getQuality());
+	}
+
+	private void updateItem(Item item){
+		//TODO
+		List<Item> items = new ArrayList<Item>();
+		items.add(item);
+		GildedRose.updateQuality(items);
+
+	}
+
+	@Test
+	public void testAgeBrie(){
+		int initialQuality = 20;
+		int initialSellin = 10;
+
+		Item item = new Item("Aged Brie", initialSellin, initialQuality);
+		this.updateItem(item);
+	    assertEquals(initialSellin-1, item.getSellIn());
+	    assertEquals(initialQuality+1, item.getQuality());
+	}
+
 
 }
